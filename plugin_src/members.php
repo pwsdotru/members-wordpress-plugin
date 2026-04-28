932aa68d943906f54e88910b552243bbef6f8fb0
Aleksandr Novikov <pwsdotru@gmail.com>
2026-04-28 23:35:32 +0300

    Init for wordpress plugin

---
+++ plugin_src/members.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Members Listing
+Text Domain: members-listing
+Plugin URI: http://pws.ru/
+Description: Plugin for control members listing on site (photo, details and etc.)
+Author: Aleksander Novikov
+Version: 1.0
+Author URI: http://pws.ru
+*/
---
+++ plugin_src/readme.txt
@@ -0,0 +1,9 @@
+=== Members Listing ===
+Contributors: pws.ru
+Tags: members, listing, gallery
+Requires at least: 6.0
+Tested up to: 6.0
+Stable tag: trunk
+Plugin for control members listing on site (photo, details and etc.)
+
+== Description ==

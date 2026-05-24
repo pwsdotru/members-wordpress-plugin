e7965a4f9b877d008aa21dea48d2722ba5c6a1f0
Aleksandr Novikov <pwsdotru@gmail.com>
2026-05-24 21:40:19 +0300

    Add support for Russian language

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 #Wordpress plugin for members listing
 
+````
+msgfmt -cv -o plugin_src/languages/members-listing-ru-RU.mo plugin_src/languages/members-listing-ru-RU.po
+````
--- plugin_src/members.php
+++ plugin_src/members.php
@@ -2,9 +2,22 @@
 /*
 Plugin Name: Members Listing
 Text Domain: members-listing
+Domain Path: /languages
 Plugin URI: http://pws.ru/
 Description: Plugin for control members listing on site (photo, details and etc.)
 Author: Aleksander Novikov
 Version: 1.0
 Author URI: http://pws.ru
 */
+
+function members_listing_load_textdomain() {
+    load_plugin_textdomain('members-listing', false, dirname(__DIR__) . "/languages");
+}
+add_action('init', 'members_listing_load_textdomain');
+
+
+//Menu
+function members_listing_menu() {
+    add_options_page(__("Members Listing", 'members-listing'), __("Members Listing", 'members-listing'), 8, __FILE__, 'members_listing_index');
+}
+add_action('admin_menu', 'members_listing_menu');
